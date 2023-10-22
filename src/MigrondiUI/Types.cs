737d296094d9511b54b18a36961d6012667a2cea
Angel D. Munoz <scythes.lol@gmail.com>
2023-10-21 19:34:13 -0600

    add helpers for workspace and project

--- src/MigrondiUI/Types.cs
+++ src/MigrondiUI/Types.cs
@@ -2,8 +2,43 @@
 
 // App Types
 
-public sealed record Project(Guid Id, string Name, Uri Path, Guid WorkspaceId);
-public sealed record Workspace(Guid Id, string Name, Uri Path, ICollection<Project> Projects);
+/// <summary>
+/// Represents a particular working set of migrations with it's own Migrondi Configuration.
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="Name">Human readable string for the user to identify the project</param>
+/// <param name="Path">Relative location to the parent's workspace</param>
+/// <param name="WorkspaceId"></param>
+public record Project(long Id, string Name, Uri Path, long WorkspaceId);
+
+
+/// <summary>
+/// It is the main organization unit for working with migrations.
+/// It contains a collection of projects.
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="Name">Human readable string for the user to identify the project</param>
+/// <param name="Path">Absolute location for this project</param>
+/// <param name="Projects"></param>
+public record Workspace(long Id, string Name, Uri Path, ICollection<Project> Projects);
+
+public static class TypeExtensions
+{
+  public static bool IsVirtual(this Project project)
+  {
+    return project.Path.Scheme == "virtual";
+  }
+
+  public static bool IsVirtual(this Workspace workspace)
+  {
+    return workspace.Path.Scheme == "virtual";
+  }
+
+  public static Uri GetProjectAbsUri(this Workspace workspace, Project project)
+  {
+    return new Uri(workspace.Path, project.Path);
+  }
+}
 
 // App Routes
 
@@ -17,10 +52,8 @@ public sealed record WorkspaceDetail(Workspace Workspace) : Route;
 
 public sealed record ProjectDetail(Project Project) : Route;
 
-
 public static class RouteExtensions
 {
-
   public static string GetName(this Route route) => route switch
   {
     Home => "Migrondi UI",
